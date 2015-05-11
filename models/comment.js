1d441d993a42a2b50dedb944a9c7f2594a4c6e36
ARR552 <ARR552@gmail.com>
2015-05-11 12:27:03 +0000

    Comentarios funcionando pero problemas con la foto en wide.css

---
+++ models/comment.js
@@ -0,0 +1,10 @@
+// Definicion del modelo de Comment con la validacion
+module.exports = function(sequelize, DataTypes){
+	return sequelize.define('Comment', 
+		{ texto: {
+		type: DataTypes.STRING,
+		validate: { notEmpty: {msg: "--> Falta el comentario"}}
+		}
+	      }
+	    );
+}
